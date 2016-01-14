4964ac333521c9368da402df41acbbecbf26c197
carapas <david_lalancette_5@hotmail.com>
2016-01-13 21:02:20 -0500

    sort to assure schedule order

--- app/pages-admin/event-schedule.jsx
+++ app/pages-admin/event-schedule.jsx
@@ -55,7 +55,7 @@ const AdminEventSchedule = React.createClass({
       return null;
     }
 
-    let hours = Object.keys(schedule.hours);
+    let hours = Object.keys(schedule.hours).sort();
     let tasks = Object.keys(schedule.hours[hours[0]]);
     let users = {};
     // @TODO
