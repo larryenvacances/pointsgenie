e2787415b19f7ee461acd1783e8cde7a6fd5d414
schmouck <simoneaujoel@gmail.com>
2016-11-17 12:56:12 -0500

    remove-description-page

--- app/layouts/UserLayout.js
+++ app/layouts/UserLayout.js
@@ -25,7 +25,6 @@ class UserLayout extends Component {
           <Nav bsStyle="pills" stacked>
             <NavItemLink to="index">Accueil</NavItemLink>
             <NavItemLink to="applications">Postulations</NavItemLink>
-            <NavItemLink to="description">Description des Postes</NavItemLink>
             <NavItemLink to="profile">Profil</NavItemLink>
             <NavItemLink to="students">Liste des étudiants</NavItemLink>
             <NavItemLink to="faq">FAQ</NavItemLink>
