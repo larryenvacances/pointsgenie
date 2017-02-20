1a5a1696f5a3078d8144478d704c36740cad353e
Guillaume Paquet <paquetgp@gmail.com>
2017-02-20 10:18:14 -0500

    Fix delete postulation when not admin

--- src/controllers/application.js
+++ src/controllers/application.js
@@ -96,7 +96,7 @@ exports.delete = function *() {
     this.throw("La postulation n'existe pas", 404);
   }
 
-  if (((currentApplication.user != this.passport.user.id) || !this.passport.user.meta.isAdmin)) {
+  if (((currentApplication.user != this.passport.user.id) && !this.passport.user.meta.isAdmin)) {
     this.throw("Seul le postulant peut supprimer sa postulation.");
   }
 
