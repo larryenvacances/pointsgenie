c259a18b16178616c416f972c6f6bd8a0888d341
Guillaume Paquet <paquetgp@gmail.com>
2017-10-20 15:37:11 -0400

    Add basic prom inscription page

---
+++ app/pages/InscriptionPage.js
@@ -0,0 +1,17 @@
+
+import React, { Component } from "react";
+import {Button} from "react-bootstrap";
+
+class InscriptionPage extends Component {
+  static displayName = "InscriptionPage";
+
+  render() {
+    return (
+      <div className="inscription">
+        <h3>Inscription</h3>
+      </div>
+    );
+  }
+};
+
+export default InscriptionPage;
